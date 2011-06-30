b68580701e6961981fe104a7c5e345c9142708aa
ebrown <devnull@localhost>
2011-06-30 18:08:45 -0400

    added a simple helper extension to wrap up Callback patterns as Observable

---
+++ EPS.Concurrency/ObservableExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace EPS.Concurrency
+{
+	class ObservableExtensions
+	{
+		public static IObservable<TResult> FromCallbackPattern<T,TResult>(T parameter, Action<T, Action<TResult>> asynchronousCall)
+		{
+			//https://groups.google.com/forum/#!topic/restsharp/R7hMDz1YRU4
+			return Observable
+				.Create<TResult>(observer =>
+				{
+					var subscribed = true;
+					asynchronousCall(parameter, value =>
+					{
+						if (!subscribed) return;
+						
+						observer.OnNext(value);
+						observer.OnCompleted();
+					});
+					
+					return () => { subscribed = false; };
+				});
+		} 
+	}
+}
