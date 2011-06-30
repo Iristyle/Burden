6805cf7ce56b15dec9afdc9cb9e7d92ca76694f7
ebrown <devnull@localhost>
2011-06-30 18:10:20 -0400

    reworked IJobQueue to make it generic, so we can at least get fed incoming params as well created generic versions of job queues

--- EPS.Concurrency/AutoJobQueue.cs
+++ EPS.Concurrency/AutoJobQueue.cs
@@ -4,6 +4,47 @@ using System.Reactive.Concurrency;
 
 namespace EPS.Concurrency
 {
+	public class AutoJobQueue<T> : ManualJobQueue<T>
+	{
+		//http://rxpowertoys.codeplex.com/
+		int maxConcurrent;
+
+		public AutoJobQueue(int maxConcurrent)
+		{
+			if (maxConcurrent < 1)
+			{
+				throw new ArgumentOutOfRangeException("maxConcurrent");
+			}
+
+			this.maxConcurrent = maxConcurrent;
+		}
+
+		public override IObservable<T> Add(Func<IObservable<T>> asyncStart)
+		{
+			if (null == asyncStart) { throw new ArgumentNullException("asyncStart"); }
+
+			return Add(asyncStart, true);
+		}
+
+		public IObservable<T> Add(Func<IObservable<T>> asyncStart, bool autoStart)
+		{
+			if (null == asyncStart) { throw new ArgumentNullException("asyncStart"); }
+
+			IObservable<T> whenCompletes = base.Add(asyncStart);
+			if (autoStart)
+				StartUpTo(maxConcurrent);
+			return whenCompletes;
+		}
+
+		protected override void OnJobCompleted(Job job, Exception error)
+		{
+			base.OnJobCompleted(job, error);
+			if (error != null)
+				Scheduler.ThreadPool.Schedule(() => StartUpTo(maxConcurrent));
+			else
+				StartUpTo(maxConcurrent);
+		}
+	}
 
 	public class AutoJobQueue : ManualJobQueue
 	{
--- EPS.Concurrency/IJobQueue.cs
+++ EPS.Concurrency/IJobQueue.cs
@@ -3,19 +3,25 @@ using System.Reactive;
 
 namespace EPS.Concurrency
 {
-	public interface IJobQueue
+	public interface IJobQueue : IJobQueue<Unit>
+	{
+		//http://rxpowertoys.codeplex.com/
+		IObservable<Unit> Add(Action action);
+	}
+
+	public interface IJobQueue<T>
 	{
 		//http://rxpowertoys.codeplex.com/
 
-		IObservable<Notification<Unit>> WhenJobCompletes { get; }
-		IObservable<Unit> WhenQueueEmpty { get; }
+		IObservable<Notification<T>> WhenJobCompletes { get; }
+		IObservable<T> WhenQueueEmpty { get; }
 		IObservable<Exception> WhenJobFails { get; }
 
 		int RunningCount { get; }
 		int QueuedCount { get; }
 
-		IObservable<Unit> Add(Action action);
-		IObservable<Unit> Add(Func<IObservable<Unit>> asyncStart);
+		IObservable<T> Add(Func<T> action);
+		IObservable<T> Add(Func<IObservable<T>> asyncStart);
 
 		bool StartNext();
 		int StartUpTo(int maxConcurrentlyRunning);
