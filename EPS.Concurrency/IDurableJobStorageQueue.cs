9af0c50ec264237229df287d126e9e772224df1d
ebrown <devnull@localhost>
2011-07-06 22:53:18 -0400

    IDurableJobStorage has been renamed to IDurableJobStorageQueue A few additional methods have been added to the interface as have docs

---
+++ EPS.Concurrency/IDurableJobStorageQueue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EPS.Concurrency
+{
+	/// <summary>	Defines a standard queue interface for durable storage of jobs. </summary>
+	/// <remarks>	7/5/2011. </remarks>
+	/// <typeparam name="TQueue">		Type of the queue item. </typeparam>
+	/// <typeparam name="TQueuePosion">	Type of the poisoned queue item. </typeparam>
+	public interface IDurableJobStorageQueue<TQueue, TQueuePoison>
+	{
+		/// <summary>	Adds a new item to the queue. </summary>
+		/// <param name="item">	The item. </param>
+		void Queue(TQueue item);
+
+		/// <summary>	Gets the next available queued item and transitions said item to the pending state. </summary>
+		/// <returns>	The item if an item was queued, otherwise null. </returns>
+		TQueue TransitionNextQueuedItemToPending();
+				
+		/// <summary>	Resets all pending items to the queued state. </summary>
+		void ResetAllPendingToQueued();
+
+		/// <summary>	Removes a queued item from the pending state. </summary>
+		/// <remarks>	Should throw an exception if no item exists in the pending state. </remarks>
+		/// <param name="item">	The item. </param>
+		void Complete(TQueue item);
+
+		/// <summary>	Poisons an item, moving it from the pending state to the poisoned state. </summary>
+		/// <remarks>	Should throw an exception if no item exists in the pending state. </remarks>
+		/// <param name="item">		   	The original item. </param>
+		/// <param name="poisonedItem">	The original item, converted to its poisoned representation. </param>
+		void Poison(TQueue item, TQueuePoison poisonedItem);
+
+		/// <summary>	Deletes the given item from the poisoned state. </summary>
+		/// <param name="poisonedItem">	The poisoned representation of an item. </param>
+		void Delete(TQueuePoison poisonedItem);
+
+		/// <summary>	Returns all poisoned items stored for this queue. </summary>
+		/// <returns>	An enumerable collection of poisoned items (that may be empty). </returns>
+		IEnumerable<TQueuePoison> GetPoisoned();
+
+		/// <summary>	Returns all queue items stored for this queue. </summary>
+		/// <returns>	An enumerable collection of queue items (that may be empty). </returns>
+		IEnumerable<TQueue> GetQueued();
+
+		/// <summary>	Returns all pending items stored for this queue. </summary>
+		/// <returns>	An enumerable collection of pending items (that may be empty). </returns>
+		IEnumerable<TQueue> GetPending();
+	}
+}
