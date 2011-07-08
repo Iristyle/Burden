0ff09f9ddb6d6826a1f5a688f3b8e0418fc8d839
ebrown <devnull@localhost>
2011-07-08 17:51:52 -0400

    made ObservableExtensions public, added docs, and throw exceptions in method wrote suite of 4 tests that pass

---
+++ EPS.Concurrency.Tests.Unit/ObservableExtensionsTest.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Reactive.Linq;
+using Xunit;
+
+namespace EPS.Concurrency.Tests.Unit
+{
+	public class ObservableExtensionsTest
+	{
+		class Foo {}
+		class FooResult 
+		{ 
+			private readonly Foo foo = null;
+			public Foo Foo { get { return foo; }}
+			public FooResult(Foo foo)
+			{
+				this.foo = foo;
+			}
+		}
+
+		class Client
+		{
+			public void SimulatedAsync(Foo foo, Action<FooResult> action)
+			{
+				action(new FooResult(foo));
+			}
+		}
+
+		[Fact]
+		public void FromCallbackPattern_ThrowsOnNullParameter()
+		{
+			Action<Foo, Action<int>> callback = (foo, action) => { };
+			Assert.Throws<ArgumentNullException>(() => ObservableExtensions.FromCallbackPattern(null as Foo, callback));
+		}
+
+		[Fact]
+		public void FromCallbackPattern_ThrowsOnNullAsynchronousCall()
+		{
+			Action<Foo, Action<int>> callback = null;
+			Assert.Throws<ArgumentNullException>(() => ObservableExtensions.FromCallbackPattern(new Foo(), callback));
+		}
+
+		[Fact]
+		public void FromCallBackPattern_FiresAsyncCallback()
+		{
+			bool fired = false;
+			Action<Foo, Action<System.Reactive.Unit>> callback = (foo, action) => { fired = true; };
+			ObservableExtensions.FromCallbackPattern(new Foo(), callback).Subscribe();
+
+			Assert.True(fired);
+		}
+
+		[Fact]
+		public void FromCallBackPattern_ExposesResultOfActionOverObservable()
+		{
+			Foo setFoo = new Foo();
+			Client client = new Client();
+		
+			FooResult result = ObservableExtensions.FromCallbackPattern(setFoo, (Foo foo, Action<FooResult> fooResult) => client.SimulatedAsync(foo, fooResult))
+				.First();
+
+			Assert.Same(setFoo, result.Foo);
+		}
+	}
+}
--- EPS.Concurrency/ObservableExtensions.cs
+++ EPS.Concurrency/ObservableExtensions.cs
@@ -5,11 +5,27 @@ using System.Reactive.Linq;
 
 namespace EPS.Concurrency
 {
-	class ObservableExtensions
+	/// <summary>	Adds some additional methods of creating Observables.  </summary>
+	/// <remarks>	7/8/2011. </remarks>
+	public class ObservableExtensions
 	{
+		/// <summary>
+		/// Creates a new observable from a standard callback pattern type API, for instance RestSharp, with ExecuteAsync(request, (response)
+		/// =&gt; {}).
+		/// Stolen from <a href="https://groups.google.com/forum/#!topic/restsharp/R7hMDz1YRU4" />
+		/// </summary>
+		/// <remarks>	7/8/2011. </remarks>
+		/// <exception cref="ArgumentNullException">	Thrown when either the parameter or asynchronousCall are null. </exception>
+		/// <typeparam name="T">	  	Generic type parameter. </typeparam>
+		/// <typeparam name="TResult">	Type of the result. </typeparam>
+		/// <param name="parameter">	   	The parameter. </param>
+		/// <param name="asynchronousCall">	The asynchronous call. </param>
+		/// <returns>	An IObservable. </returns>
 		public static IObservable<TResult> FromCallbackPattern<T,TResult>(T parameter, Action<T, Action<TResult>> asynchronousCall)
 		{
-			//https://groups.google.com/forum/#!topic/restsharp/R7hMDz1YRU4
+			if (null == parameter) { throw new ArgumentNullException("parameter"); }
+			if (null == asynchronousCall) { throw new ArgumentNullException("asynchronousCall"); }
+
 			return Observable
 				.Create<TResult>(observer =>
 				{
