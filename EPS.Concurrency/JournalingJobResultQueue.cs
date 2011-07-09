f6f59eedc604cfd14f9bac0436d77c3a4785f685
ebrown <devnull@localhost>
2011-07-09 00:03:21 -0400

    added logger / log errors on null notifications, invalid inspect results, unknown job queue action reorderd constructor params / renamed whenJobCompletes to jobCompletionNotifications wrote xml docs

--- EPS.Concurrency/JournalingJobResultQueue.cs
+++ EPS.Concurrency/JournalingJobResultQueue.cs
@@ -1,26 +1,48 @@
 using System;
 using System.Reactive;
+using Common.Logging;
 
 namespace EPS.Concurrency
 {
-	public class JournalingJobResultQueue<TQueue, TQueueResult, TQueuePoison>
+	/// <summary>
+	/// This class journals the results of an in memory observable job publisher to a backing store for durability, using a custom job result
+	/// inspector to determine if the original queue item should be removed or poisoned.
+	/// </summary>
+	/// <remarks>	7/8/2011. </remarks>
+	/// <typeparam name="TJobInput">   	The type of the input to the job. </typeparam>
+	/// <typeparam name="TJobOutput">  	The type of the output from the job. </typeparam>
+	/// <typeparam name="TQueuePoison">	Type to be stored if a job failed and should be poisoned. </typeparam>
+	public class JournalingJobResultQueue<TJobInput, TJobOutput, TQueuePoison>
 	{
 		private IDisposable jobCompleted;
 
-		public JournalingJobResultQueue(IDurableJobStorageQueue<TQueue, TQueuePoison> durableJobStorage, 
-			IJobResultInspector<TQueue, TQueueResult, TQueuePoison> jobResultInspector,
-			IObservable<Notification<JobResult<TQueue, TQueueResult>>> whenJobCompletes)
+		//TODO: 7-8-2011 -- should we property inject this from our container?
+		private ILog log = LogManager.GetCurrentClassLogger();
+
+		public JournalingJobResultQueue(IObservable<Notification<JobResult<TJobInput, TJobOutput>>> jobCompletionNotifications, 
+			IJobResultInspector<TJobInput, TJobOutput, TQueuePoison> jobResultInspector,
+			IDurableJobStorageQueue<TJobInput, TQueuePoison> durableJobStorage)
 		{
 			if (null == durableJobStorage) { throw new ArgumentNullException("durableJobStorage"); }
 			if (null == jobResultInspector) { throw new ArgumentNullException("jobResultInspector"); }
-			if (null == whenJobCompletes) { throw new ArgumentNullException("whenJobCompletes"); }
+			if (null == jobCompletionNotifications) { throw new ArgumentNullException("jobCompletionNotifications"); }
 
-			this.jobCompleted = whenJobCompletes
+			this.jobCompleted = jobCompletionNotifications
 			.Subscribe(notification =>
 			{
+				if (null == notification)
+				{
+					log.Error(m => m("Received invalid NULL Notification<JobResult<{0},{1}>>", typeof(TJobInput).Name, typeof(TJobOutput).Name));
+					return;
+				}
+
 				var queueAction = jobResultInspector.Inspect(notification);
 
-				if (queueAction.ActionType == JobQueueActionType.Poison)
+				if (null == queueAction)
+				{
+					log.Error(m => m("Received invalid NULL JobQueueAction<{0}> from Inspect call", typeof(TQueuePoison).Name));
+				}
+				else if (queueAction.ActionType == JobQueueActionType.Poison)
 				{
 					durableJobStorage.Poison(notification.Value.Input, queueAction.QueuePoison);
 				}
@@ -30,7 +52,7 @@ namespace EPS.Concurrency
 				}
 				else
 				{
-					//TODO: 7-8-2011 -- we should probably be logging this off somewhere
+					log.Error(m => m("Received invalid JobQueueAction<{0}> with JobQueueActionType of Unknown", typeof(TQueuePoison).Name));
 				}
 			});
 		}
