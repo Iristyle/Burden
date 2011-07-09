7c463cf56f25f33b55a9c9d18c23e9ad6bd1c2d8
ebrown <devnull@localhost>
2011-07-09 11:09:31 -0400

    completed a suite of tests on JournalingJobResultQueue to make sure it properly journals give specific inputs wrote docs added ability to inject logger and scheduler via internal constructor for testing

--- EPS.Concurrency.Tests.Unit/JournalingJobResultQueueTest.cs
+++ EPS.Concurrency.Tests.Unit/JournalingJobResultQueueTest.cs
@@ -1,8 +1,13 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reactive;
+using System.Reactive.Concurrency;
 using System.Reactive.Linq;
+using Common.Logging;
 using FakeItEasy;
 using Xunit;
+using Xunit.Extensions;
 
 namespace EPS.Concurrency.Tests.Unit
 {
@@ -33,13 +38,48 @@ namespace EPS.Concurrency.Tests.Unit
 		}
 
 		[Fact]
+		public void Subscribe_IgnoresNullNotifications()
+		{
+			var durableJobStorage = A.Fake<IDurableJobStorageQueue<int, object>>();
+			var jobResultInspector = A.Fake<IJobResultInspector<int, int, object>>();
+			var items = new Notification<JobResult<int, int>>[]
+			{
+				null,
+				Notification.CreateOnNext(new JobResult<int, int>(1, 1)),
+				null,
+				null,
+				Notification.CreateOnNext(new JobResult<int, int>(1, 1))
+			};
+			var observable = items.ToObservable();
+			var queue = new JournalingJobResultQueue<int, int, object>(observable, jobResultInspector, durableJobStorage, A.Fake<ILog>(), Scheduler.Immediate);
+
+			A.CallTo(() => jobResultInspector.Inspect(A<Notification<JobResult<int, int>>>.Ignored)).MustHaveHappened(Repeated.Exactly.Times(items.Count(n => null != n)));
+		}
+
+		[Fact]
+		public void Subscribe_NullNotificationTriggersLogging()
+		{
+			var durableJobStorage = A.Fake<IDurableJobStorageQueue<int, object>>();
+			var jobResultInspector = A.Fake<IJobResultInspector<int, int, object>>();
+			var observable = new Notification<JobResult<int, int>>[]
+			{ null
+			}.ToObservable();
+			var log = A.Fake<ILog>();
+
+			var queue = new JournalingJobResultQueue<int, int, object>(observable, jobResultInspector, durableJobStorage, log, Scheduler.Immediate);
+
+			A.CallTo(() => log.Error(A<Action<FormatMessageHandler>>.Ignored)).MustHaveHappened();
+		}
+
+		[Fact]
 		public void Subscribe_EmptyObservableNeverTriggersInspection()
 		{
 			var durableJobStorage = A.Fake<IDurableJobStorageQueue<int, object>>();
 			var jobResultInspector = A.Fake<IJobResultInspector<int, int, object>>();
-			var observable = new Notification<JobResult<int, int>>[] { null }.ToObservable();
+			var observable = new Notification<JobResult<int, int>>[]
+			{ }.ToObservable();
 
-			var queue = new JournalingJobResultQueue<int, int, object>(observable, jobResultInspector, durableJobStorage);
+			var queue = new JournalingJobResultQueue<int, int, object>(observable, jobResultInspector, durableJobStorage, A.Fake<ILog>(), Scheduler.Immediate);
 
 			A.CallTo(() => jobResultInspector.Inspect(A<Notification<JobResult<int, int>>>.Ignored)).MustNotHaveHappened();
 		}
@@ -50,13 +90,156 @@ namespace EPS.Concurrency.Tests.Unit
 			var durableJobStorage = A.Fake<IDurableJobStorageQueue<int, object>>();
 			var jobResultInspector = A.Fake<IJobResultInspector<int, int, object>>();
 			var observable = new []
-			{ 
+			{
 				Notification.CreateOnNext(new JobResult<int, int>(1, 1))
 			}.ToObservable();
 
-			var queue = new JournalingJobResultQueue<int, int, object>(observable, jobResultInspector, durableJobStorage);
+			var queue = new JournalingJobResultQueue<int, int, object>(observable, jobResultInspector, durableJobStorage, A.Fake<ILog>(), Scheduler.Immediate);
 
 			A.CallTo(() => jobResultInspector.Inspect(A<Notification<JobResult<int, int>>>.Ignored)).MustHaveHappened(Repeated.Exactly.Once);
+		}
+
+		class JobDetails
+		{
+			public IDurableJobStorageQueue<int, object> DurableStorage { get; set; }
+			public JournalingJobResultQueue<int, int, object> JournalingJobResultQueue { get; set; }
+			public IJobResultInspector<int, int, object> Inspector { get; set; }
+			public ILog Log { get; set; }
+		}
+
+		private static JobDetails SimulateInspectionResultsToMonitorDurableJobStorageBehavior(JobQueueAction<object> [] inspectionResults)
+		{
+			var details = new JobDetails()
+			{
+				DurableStorage = A.Fake<IDurableJobStorageQueue<int, object>>(),
+				Inspector = A.Fake<IJobResultInspector<int, int, object>>(),
+				Log = A.Fake<ILog>()
+			};
+			var observable = Enumerable.Repeat(Notification.CreateOnNext(new JobResult<int, int>(1, 1)), inspectionResults.Length)
+			.ToObservable();
+
+			A.CallTo(() => details.Inspector.Inspect(A<Notification<JobResult<int, int>>>.Ignored)).ReturnsNextFromSequence(inspectionResults);
+
+			details.JournalingJobResultQueue = new JournalingJobResultQueue<int, int, object>(observable, details.Inspector, details.DurableStorage, details.Log, Scheduler.CurrentThread);
+			
+			return details;
+		}
+
+		public static IEnumerable<object[]> GetExpectedCompletionCountsForJobQueueAction
+		{
+			get
+			{
+				yield return new object[] { Enumerable.Repeat(null as JobQueueAction<object>, 5), 0 };
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(), 5), 5 };
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(new object()), 3), 0 };
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Poison), 12), 0 };
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Unknown), 3), 0 };
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Complete), 15), 15 };
+				//mix'n'match
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Complete), 15)
+					.Concat(Enumerable.Repeat(new JobQueueAction<object>(new object()), 3))
+					.Concat(Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Unknown), 4))
+					.Concat(Enumerable.Repeat(new JobQueueAction<object>(), 2))
+					.Concat(Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Poison), 1))
+					.Concat(Enumerable.Repeat(null as JobQueueAction<object>, 2))
+					, 17 };
+			}
+		}
+
+		[Theory]
+		[PropertyData("GetExpectedCompletionCountsForJobQueueAction")]
+		public void Subscribe_JobQueueActionOfTypeTriggersCorrectNumberOfCallsToDurableJobQueueComplete(IEnumerable<JobQueueAction<object>> jobQueueActions, int expectedCalls)
+		{
+			var jobDetails = SimulateInspectionResultsToMonitorDurableJobStorageBehavior(jobQueueActions.ToArray());
+			A.CallTo(() => jobDetails.DurableStorage.Complete(A<int>.Ignored)).MustHaveHappened(Repeated.Exactly.Times(expectedCalls));
+		}
+
+		public static IEnumerable<object[]> GetExpectedPoisonCountsForJobQueueAction
+		{
+			get
+			{
+				yield return new object[] { Enumerable.Repeat(null as JobQueueAction<object>, 5), 0 };
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(), 5), 0 };
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(new object()), 3), 3 };
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Poison), 12), 12 };
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Unknown), 3), 0 };
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Complete), 15), 0 };
+				//mix'n'match
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Complete), 15)
+					.Concat(Enumerable.Repeat(new JobQueueAction<object>(new object()), 3))
+					.Concat(Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Unknown), 4))
+					.Concat(Enumerable.Repeat(new JobQueueAction<object>(), 2))
+					.Concat(Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Poison), 1))
+					.Concat(Enumerable.Repeat(null as JobQueueAction<object>, 2))
+					, 4 };
+				
+			}
+		}
+
+		[Theory]
+		[PropertyData("GetExpectedPoisonCountsForJobQueueAction")]
+		public void Subscribe_JobQueueActionOfTypeTriggersCorrectNumberOfCallsToDurableJobQueuePoison(IEnumerable<JobQueueAction<object>> jobQueueActions, int expectedCalls)
+		{
+			var jobDetails = SimulateInspectionResultsToMonitorDurableJobStorageBehavior(jobQueueActions.ToArray());
+			A.CallTo(() => jobDetails.DurableStorage.Poison(A<int>.Ignored, A<object>.Ignored)).MustHaveHappened(Repeated.Exactly.Times(expectedCalls));
+		}
+
+		public static IEnumerable<object[]> GetExpectedDeleteCountsForJobQueueAction
+		{
+			get
+			{
+				yield return new object[] { Enumerable.Repeat(null as JobQueueAction<object>, 5), 0 };
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(), 5), 0 };
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(new object()), 3), 0 };
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Poison), 12), 0 };
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Unknown), 3), 0 };
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Complete), 15), 0 };
+				//mix'n'match
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Complete), 15)
+					.Concat(Enumerable.Repeat(new JobQueueAction<object>(new object()), 3))
+					.Concat(Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Unknown), 4))
+					.Concat(Enumerable.Repeat(new JobQueueAction<object>(), 2))
+					.Concat(Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Poison), 1))
+					.Concat(Enumerable.Repeat(null as JobQueueAction<object>, 2))
+					, 0 };
+			}
+		}
+
+		[Theory]
+		[PropertyData("GetExpectedDeleteCountsForJobQueueAction")]
+		public void Subscribe_JobQueueActionOfTypeTriggersCorrectNumberOfCallsToDurableJobQueueDelete(IEnumerable<JobQueueAction<object>> jobQueueActions, int expectedCalls)
+		{
+			var jobDetails = SimulateInspectionResultsToMonitorDurableJobStorageBehavior(jobQueueActions.ToArray());
+			A.CallTo(() => jobDetails.DurableStorage.Delete(A<object>.Ignored)).MustHaveHappened(Repeated.Exactly.Times(expectedCalls));
+		}
+
+		public static IEnumerable<object[]> GetExpectedLogCountsForJobQueueAction
+		{
+			get
+			{
+				yield return new object[] { Enumerable.Repeat(null as JobQueueAction<object>, 5), 5 };
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(), 5), 0 };
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(new object()), 3), 0 };
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Poison), 12), 0 };
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Unknown), 3), 3 };
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Complete), 15), 0 };
+				//mix'n'match
+				yield return new object[] { Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Complete), 15)
+					.Concat(Enumerable.Repeat(new JobQueueAction<object>(new object()), 3))
+					.Concat(Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Unknown), 4))
+					.Concat(Enumerable.Repeat(new JobQueueAction<object>(), 2))
+					.Concat(Enumerable.Repeat(new JobQueueAction<object>(JobQueueActionType.Poison), 1))
+					.Concat(Enumerable.Repeat(null as JobQueueAction<object>, 2))
+					, 6 };
+			}
+		}
+
+		[Theory]
+		[PropertyData("GetExpectedLogCountsForJobQueueAction")]
+		public void Subscribe_JobQueueActionOfTypeTriggersCorrectNumberOfErrorLogCalls(IEnumerable<JobQueueAction<object>> jobQueueActions, int expectedCalls)
+		{
+			var jobDetails = SimulateInspectionResultsToMonitorDurableJobStorageBehavior(jobQueueActions.ToArray());
+			A.CallTo(() => jobDetails.Log.Error(A<Action<FormatMessageHandler>>.Ignored)).MustHaveHappened(Repeated.Exactly.Times(expectedCalls));
 		}
 	}
 }
--- EPS.Concurrency/JournalingJobResultQueue.cs
+++ EPS.Concurrency/JournalingJobResultQueue.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
 using Common.Logging;
 
 namespace EPS.Concurrency
 {
 	/// <summary>
 	/// This class journals the results of an in memory observable job publisher to a backing store for durability, using a custom job result
-	/// inspector to determine if the original queue item should be removed or poisoned.
+	/// inspector to determine if the original queue item should be removed or poisoned.  Bad notifications or inspection results are published to the logger.
 	/// </summary>
 	/// <remarks>	7/8/2011. </remarks>
 	/// <typeparam name="TJobInput">   	The type of the input to the job. </typeparam>
@@ -16,18 +18,30 @@ namespace EPS.Concurrency
 	{
 		private IDisposable jobCompleted;
 
-		//TODO: 7-8-2011 -- should we property inject this from our container?
-		private ILog log = LogManager.GetCurrentClassLogger();
-
+		/// <summary>	Constructs a new instance. </summary>
+		/// <remarks>	7/9/2011. </remarks>
+		/// <param name="jobCompletionNotifications">	The job completion notification stream. </param>
+		/// <param name="jobResultInspector">		 	The job result inspector. </param>
+		/// <param name="durableJobStorage">		 	The durable job storage. </param>
 		public JournalingJobResultQueue(IObservable<Notification<JobResult<TJobInput, TJobOutput>>> jobCompletionNotifications, 
 			IJobResultInspector<TJobInput, TJobOutput, TQueuePoison> jobResultInspector,
-			IDurableJobStorageQueue<TJobInput, TQueuePoison> durableJobStorage)
+			IDurableJobStorageQueue<TJobInput, TQueuePoison> durableJobStorage) :
+			this(jobCompletionNotifications, jobResultInspector, durableJobStorage, LogManager.GetCurrentClassLogger(), Scheduler.TaskPool)
+		{ }
+
+		internal JournalingJobResultQueue(IObservable<Notification<JobResult<TJobInput, TJobOutput>>> jobCompletionNotifications, 
+			IJobResultInspector<TJobInput, TJobOutput, TQueuePoison> jobResultInspector,
+			IDurableJobStorageQueue<TJobInput, TQueuePoison> durableJobStorage,
+			ILog log,
+			IScheduler scheduler)
 		{
+
 			if (null == durableJobStorage) { throw new ArgumentNullException("durableJobStorage"); }
 			if (null == jobResultInspector) { throw new ArgumentNullException("jobResultInspector"); }
 			if (null == jobCompletionNotifications) { throw new ArgumentNullException("jobCompletionNotifications"); }
 
 			this.jobCompleted = jobCompletionNotifications
+			.SubscribeOn(scheduler)
 			.Subscribe(notification =>
 			{
 				if (null == notification)
@@ -42,6 +56,7 @@ namespace EPS.Concurrency
 				{
 					log.Error(m => m("Received invalid NULL JobQueueAction<{0}> from Inspect call", typeof(TQueuePoison).Name));
 				}
+				//no need to check 
 				else if (queueAction.ActionType == JobQueueActionType.Poison)
 				{
 					durableJobStorage.Poison(notification.Value.Input, queueAction.QueuePoison);
