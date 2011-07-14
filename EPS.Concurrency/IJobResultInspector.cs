d8232687f5c9fdf11c0a3c1738a4676c77f7161b
ebrown <devnull@localhost>
2011-07-14 01:50:27 -0400

    tweaked docs with some notes about expectations

--- EPS.Concurrency/IJobResultInspector.cs
+++ EPS.Concurrency/IJobResultInspector.cs
@@ -7,10 +7,11 @@ namespace EPS.Concurrency
 	/// <remarks>	7/8/2011. </remarks>
 	/// <typeparam name="TJobInput">	Type of the job input. </typeparam>
 	/// <typeparam name="TJobInput">	Type of the job output. </typeparam>
-	/// <typeparam name="TJobInput">	Type of a poisioned job. </typeparam>
+	/// <typeparam name="TJobInput">	Type of a poisoned job. </typeparam>
 	public interface IJobResultInspector<TJobInput, TJobOutput, TQueuePoison>
 	{
 		/// <summary>	Inspects an Rx Notification of JobResult, determining if the result should cause a queue poison or completion. </summary>
+		/// <remarks> If the Notification is an error, the exception passed in should be of type </remarks>
 		/// <param name="jobResult">	The job result. </param>
 		/// <returns>	A JobQueueAction specifying what to do about the JobResult. </returns>
 		/// <exception cref="ArgumentNullException">	Should throw on a null Notification. </exception>
