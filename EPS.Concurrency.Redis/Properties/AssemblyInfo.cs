b03f24c81c5424588b7fdf6d6556ba0a907cccda
ebrown <devnull@localhost>
2011-07-24 22:45:04 -0400

    Added new projects to hold Redis Concurrency classes ran both regular and test project through build integration scripts

---
+++ EPS.Concurrency.Redis/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EPS.Concurrency.Redis")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("EPS.Concurrency.Redis")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5ef0568f-1527-4acb-811e-4c5bb7ef8e07")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
