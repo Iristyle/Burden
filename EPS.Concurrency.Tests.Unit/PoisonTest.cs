0f8d455b3733c46dfc1867c3f52737af968a9799
ebrown <devnull@localhost>
2011-07-24 22:36:23 -0400

    new Poison class that can be used to easily wrap input types with an exception wrote passing tests

---
+++ EPS.Concurrency.Tests.Unit/PoisonTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using EPS.Utility;
+using Xunit;
+
+namespace EPS.Concurrency.Tests.Unit
+{
+	public class PoisonTest
+	{
+		[Fact]
+		public void Constructor_Throws_OnNullInput_ForReferenceType()
+		{
+			Assert.Throws<ArgumentNullException>(() => new Poison<string>(null, new ArgumentNullException()));
+		}
+
+		[Fact]
+		public void Constructor_Throws_OnNullException()
+		{
+			Assert.Throws<ArgumentNullException>(() => new Poison<string>("foo", null));
+		}
+		
+		[Fact]
+		public void Constructor_StoresValueType()
+		{
+			int value = 42;
+			var poison = new Poison<int>(value, new ArgumentNullException());
+
+			Assert.Equal(value, poison.Input);
+		}
+
+		[Fact]
+		public void Constructor_StoresReferenceType()
+		{
+			string value = "test";
+			var poison = new Poison<string>(value, new ArgumentNullException());
+
+			Assert.Equal(value, poison.Input);
+		}
+
+		[Fact]
+		public void Constructor_StoresException()
+		{
+			var exception = new ArgumentNullException("foo");
+			var poison = new Poison<int>(42, exception);
+
+			Assert.Equal(new ExceptionDetails(exception), poison.Exception, GenericEqualityComparer<ExceptionDetails>.ByAllMembers());
+		}
+	}
+}
---
+++ EPS.Concurrency/Poison.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace EPS.Concurrency
+{
+	/// <summary>	A generic class representing an input annotated with an Exception.  </summary>
+	/// <remarks>	7/24/2011. </remarks>
+	/// <typeparam name="T">	The Type of the input. </typeparam>
+	public class Poison<T>
+	{
+		/// <summary>	Gets the input. </summary>
+		/// <value>	The input. </value>
+		public T Input { get; private set; }
+		
+		/// <summary>	Gets theexception details. </summary>
+		/// <value>	The exception. </value>
+		public ExceptionDetails Exception { get; private set; }
+		
+		/// <summary>	Constructor. </summary>
+		/// <remarks>	7/24/2011. </remarks>
+		/// <exception cref="ArgumentNullException">	Thrown when T is a reference type and input is null OR when exception is null. </exception>
+		/// <param name="input">		The input. </param>
+		/// <param name="exception">	The exception, which will be wrapped up as an ExceptionDetails. </param>
+		public Poison(T input, Exception exception)
+		{
+			if (!typeof(T).IsValueType && object.ReferenceEquals(input, null))
+			{
+				throw new ArgumentNullException("input");
+			}
+			if (null == exception) { throw new ArgumentNullException("exception"); }
+
+			this.Input = input;
+			this.Exception = new ExceptionDetails(exception);
+		}
+	}
+}
