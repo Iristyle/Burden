06882e4ffe22ce88ce4167fe6a2affb7941d13fb
ebrown <devnull@localhost>
2011-07-26 09:28:31 -0400

    added a simple way to set queue names added full test suite for this type using AutoFixture in Redis integration tests

---
+++ EPS.Concurrency.Redis.Tests.Integration/QueueNamesTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Ploeh.AutoFixture;
+using Xunit;
+
+namespace EPS.Concurrency.Redis.Tests.Integration
+{
+	public class QueueNamesTest
+	{
+		private IFixture fixture = new Fixture();
+
+		[Fact]
+		public void Default_QueueNameIsExpected()
+		{
+			//this is a contract with how names are stored in Redis so shouldn't change
+			Assert.Equal("request", QueueNames.Default.Request);
+		}
+
+		[Fact]
+		public void Default_PendingNameIsExpected()
+		{
+			//this is a contract with how names are stored in Redis so shouldn't change
+			Assert.Equal("pending", QueueNames.Default.Pending);
+		}
+
+		[Fact]
+		public void Default_PoisonNameIsExpected()
+		{
+			//this is a contract with how names are stored in Redis so shouldn't change
+			Assert.Equal("poison", QueueNames.Default.Poison);
+		}
+
+		[Fact]
+		public void Constructor_Throws_OnNullRequestQueueName()
+		{
+			Assert.Throws<ArgumentNullException>(() => new QueueNames(null, fixture.CreateAnonymous<string>(), fixture.CreateAnonymous<
+			string>()));
+		}
+
+		[Fact]
+		public void Constructor_Throws_OnNullPendingQueueName()
+		{
+			Assert.Throws<ArgumentNullException>(() => new QueueNames(fixture.CreateAnonymous<string>(), null, fixture.CreateAnonymous<
+			string>()));
+		}
+
+		[Fact]
+		public void Constructor_Throws_OnNullPoisonQueueName()
+		{
+			Assert.Throws<ArgumentNullException>(() => new QueueNames(fixture.CreateAnonymous<string>(), fixture.CreateAnonymous<string>(), null));
+		}
+
+		[Fact]
+		public void Constructor_ReturnsCorrectRequestQueueName()
+		{
+			var name = fixture.CreateAnonymous<string>();
+			var names = new QueueNames(name, fixture.CreateAnonymous<string>(), fixture.CreateAnonymous<string>());
+			Assert.Equal(name, names.Request);
+		}
+
+		[Fact]
+		public void Constructor_ReturnsCorrectPendingQueueName()
+		{
+			var name = fixture.CreateAnonymous<string>();
+			var names = new QueueNames(fixture.CreateAnonymous<string>(), name, fixture.CreateAnonymous<string>());
+			Assert.Equal(name, names.Pending);
+		}
+
+		[Fact]
+		public void Constructor_ReturnsCorrectPoisonQueueName()
+		{
+			var name = fixture.CreateAnonymous<string>();
+			var names = new QueueNames(fixture.CreateAnonymous<string>(), fixture.CreateAnonymous<string>(), name);
+			Assert.Equal(name, names.Poison);
+		}
+	}
+}
---
+++ EPS.Concurrency.Redis/QueueNames.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace EPS.Concurrency.Redis
+{
+	/// <summary>	A simple class that keeps the various queue names together.  </summary>
+	/// <remarks>	6/28/2011. </remarks>
+	public class QueueNames
+	{
+		private static QueueNames _default = new QueueNames("request", "pending", "poison");
+		/// <summary>	Gets the name of the request queue. </summary>
+		/// <value>	The request queue name. </value>
+		public string Request { get; private set; }
+
+		/// <summary>	Gets the name of the pending queue. </summary>
+		/// <value>	The pending queue name. </value>
+		public string Pending { get; private set; }
+
+		/// <summary>	Gets the name of the poison queue. </summary>
+		/// <value>	The poison queue name. </value>
+		public string Poison { get; private set; }
+		
+		/// <summary>
+		/// Initializes a new instance of the QueueNames class.
+		/// </summary>
+		public QueueNames(string request, string pending, string poison)
+		{
+			if (null == request) { throw new ArgumentNullException("request"); }
+			if (string.IsNullOrWhiteSpace(request)) { throw new ArgumentException("must not be whitespace", "request"); }
+
+			if (null == pending) { throw new ArgumentNullException("pending"); }
+			if (string.IsNullOrWhiteSpace(pending)) { throw new ArgumentException("must not be whitespace", "pending"); }
+	
+			if (null == poison) { throw new ArgumentNullException("poison"); }
+			if (string.IsNullOrWhiteSpace(poison)) { throw new ArgumentException("must not be whitespace", "poison"); }
+
+			Request = request;
+			Poison = poison;
+			Pending = pending;
+		}
+
+		/// <summary>	Gets the default queue names of 'request', 'pending' and 'poison'. </summary>
+		/// <value>	The default. </value>
+		public static QueueNames Default 
+		{
+			get { return _default; }
+		}
+	}
+}
