0c3297b70ce5430f4454bb99191a0ca08aebc154
ebrown <devnull@localhost>
2011-07-27 03:36:42 -0400

    IMonitoredJobQueue has been tweaked to allow publishing some additional info over IObservable RunningCount / QueuedCount surfaced from the job execution queue OnJobCompletion surfaced from job execution queue OnQueueAction surfaced from ObservableDurableJobQueue added TimeSpan to CancelQueuedAndWaitForExecutingJobsToComplete

--- EPS.Concurrency/IMonitoredJobQueue.cs
+++ EPS.Concurrency/IMonitoredJobQueue.cs
@@ -5,16 +5,37 @@ namespace EPS.Concurrency
 	/// <summary>	A very simple interface for adding items to a monitored job queue.  </summary>
 	/// <remarks>	7/24/2011. </remarks>
 	/// <typeparam name="T">	Generic type parameter. </typeparam>
-	public interface IMonitoredJobQueue<T>
+	public interface IMonitoredJobQueue<TJobInput, TJobOutput, TPoison>
 		: IDisposable
 	{
 		/// <summary>	Adds a job.  </summary>
 		/// <param name="input">	The input. </param>
-		void AddJob(T input);
-		
+		void AddJob(TJobInput input);
+
+		/// <summary>
+		/// The Observable that monitors job completion, where completion can be either run to completion, exception or cancellation from the job
+		/// execution queue.
+		/// </summary>
+		/// <value>	A sequence of observable job completion notifications from the job execution queue. </value>
+		IObservable<JobResult<TJobInput, TJobOutput>> OnJobCompletion { get; }
+
+		/// <summary>	Gets notifications as items are moved around the durable queue. </summary>
+		/// <value>	A sequence of observable durable queue notifications. </value>
+		IObservable<DurableJobQueueAction<TJobInput, TPoison>> OnQueueAction { get; }
 
 		/// <summary>	Cancel queued jobs and wait for executing jobs to complete. </summary>
-		/// <remarks>	This should automatically be called upon Dispose. </remarks>
-		void CancelQueuedAndWaitForExecutingJobsToComplete();
+		/// <remarks>	This should automatically be called upon Dispose by classes implementing this interface. </remarks>
+		/// <param name="timeSpan">	The time span to block until terminating. </param>
+		void CancelQueuedAndWaitForExecutingJobsToComplete(TimeSpan timeout);
+
+		/// <summary>	Gets the number of running jobs from the job execution queue. </summary>
+		/// <value>	The number of running jobs. </value>
+		int RunningCount { get; }
+
+		/// <summary>	Gets the number of queued jobs from the job execution queue. </summary>
+		/// <value>	The number of queued jobs. </value>
+		int QueuedCount { get; }
+
+		//TODO: 7-26-2011 -- consider surfacing additional observables from a wrapped IJobResultInspector
 	}
 }
