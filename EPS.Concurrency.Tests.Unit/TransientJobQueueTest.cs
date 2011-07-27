b3a9ad186b0dfcaa9e30ca1921dd286bc863c47d
ebrown <devnull@localhost>
2011-07-27 02:37:16 -0400

    TestJobQueue moved out of test project and into EPS.Concurrency/TransientJobQueue this class was needed to model some in memory tests, etc not advised to use the implementation, but its there for reference, use of other tests, etc wrote docs test suite passes

---
+++ EPS.Concurrency.Tests.Unit/TransientJobQueueTest.cs
@@ -0,0 +1,42 @@
+using System;
+using EPS.Utility;
+using Xunit;
+
+namespace EPS.Concurrency.Tests.Unit
+{
+	public class TransientValueTypeJobQueueTest
+	: TransientJobQueueTest<int, int>
+	{
+		public TransientValueTypeJobQueueTest()
+			: base(input => 3)
+		{ }
+	}
+
+	public class TransientReferenceTypeJobQueueTest
+		: TransientJobQueueTest<string, string>
+	{
+		public TransientReferenceTypeJobQueueTest()
+			: base(input => "foo")
+		{ }
+	}
+
+	public abstract class TransientJobQueueTest<TQueue, TQueuePoison>
+		: IDurableJobQueueTest<TransientJobQueue<TQueue, TQueuePoison>, TQueue, TQueuePoison>
+	{
+		public TransientJobQueueTest(Func<TQueue, TQueuePoison> poisonConverter)
+			: base(() => new TransientJobQueue<TQueue, TQueuePoison>(GenericEqualityComparer<TQueue>.ByAllMembers(), GenericEqualityComparer<TQueuePoison>.ByAllMembers()), poisonConverter)
+		{ }
+
+		[Fact]
+		public void Constructor_Throws_OnNullItemComparer()
+		{
+			Assert.Throws<ArgumentNullException>(() => new TransientJobQueue<TQueue, TQueuePoison>(null, GenericEqualityComparer<TQueuePoison>.ByAllMembers()));
+		}
+
+		[Fact]
+		public void Constructor_Throws_OnNullPoisonComparer()
+		{
+			Assert.Throws<ArgumentNullException>(() => new TransientJobQueue<TQueue, TQueuePoison>(GenericEqualityComparer<TQueue>.ByAllMembers(), null));
+		}
+	}
+}
---
+++ EPS.Concurrency/TransientJobQueue.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EPS.Concurrency
+{
+	/// <summary>	Queue of transient jobs, with no backing store. Provided only for testing and demonstration purposes. </summary>
+	/// <remarks>	7/27/2011.  Performance of this class will be terrible. </remarks>
+	/// <typeparam name="TQueue">	   	Type of the queue. </typeparam>
+	/// <typeparam name="TQueuePoison">	Type of the queue poison. </typeparam>
+	public class TransientJobQueue<TQueue, TQueuePoison>
+	: IDurableJobQueue<TQueue, TQueuePoison>
+	{
+		private List<TQueue> queue = new List<TQueue>();
+		private List<TQueue> pending = new List<TQueue>();
+		private List<TQueuePoison> poisoned = new List<TQueuePoison>();
+		private IEqualityComparer<TQueue> itemComparer;
+		private IEqualityComparer<TQueuePoison> poisonComparer;
+
+		/// <summary>	Constructor. </summary>
+		/// <remarks>	7/27/2011. </remarks>
+		/// <exception cref="ArgumentNullException">	Thrown when either the itemComparer or poisonComparer are null. </exception>
+		/// <param name="itemComparer">  	The item comparer. </param>
+		/// <param name="poisonComparer">	The poison comparer. </param>
+		public TransientJobQueue(IEqualityComparer<TQueue> itemComparer, IEqualityComparer<TQueuePoison> poisonComparer)
+		{
+			if (null == itemComparer) { throw new ArgumentNullException("itemComparer"); }
+			if (null == poisonComparer) { throw new ArgumentNullException("poisonComparer"); }
+
+			this.itemComparer = itemComparer;
+			this.poisonComparer = poisonComparer;
+		}
+
+		/// <summary>	Adds a new item to the queue. </summary>
+		/// <remarks>	7/27/2011. </remarks>
+		/// <exception cref="ArgumentNullException">	Thrown when the item is null. </exception>
+		/// <param name="item">	The item. </param>
+		public void Queue(TQueue item)
+		{
+			if (null == item) { throw new ArgumentNullException("item"); }
+
+			lock (queue)
+			{
+				queue.Add(item);
+			}
+		}
+
+		/// <summary>	Gets the next available queued item and transitions said item to the pending state. </summary>
+		/// <returns>	The item if an item was queued, otherwise null. </returns>
+		public TQueue TransitionNextQueuedItemToPending()
+		{
+			lock (queue)
+			{
+				if (queue.Count == 0)
+					return default(TQueue);
+
+				var item = queue.ElementAt(0);
+				queue.RemoveAt(0);
+
+				pending.Add(item);
+				return item;
+			}
+		}
+
+		/// <summary>	Resets all pending items to the queued state. </summary>
+		public void ResetAllPendingToQueued()
+		{
+			lock (queue)
+			{
+				if (pending.Count == 0)
+					return;
+
+				queue = pending.Concat(queue).ToList();
+				pending.Clear();
+			}
+		}
+
+		/// <summary>	Removes a queued item from the pending state. </summary>
+		/// <remarks>	Should throw an exception if no item exists in the pending state. </remarks>
+		/// <exception cref="ArgumentNullException">	Thrown when the item is null. </exception>
+		/// <param name="item">	The item. </param>
+		/// <returns>	true if it succeeds, false if it fails. </returns>
+		public bool Complete(TQueue item)
+		{
+			if (null == item) { throw new ArgumentNullException("item"); }
+
+			lock (queue)
+			{
+				int index = pending.FindIndex(p => itemComparer.Equals(p, item));
+				if (-1 == index)
+				{
+					return false;
+				}
+				pending.RemoveAt(index);
+				return true;
+			}
+		}
+
+		/// <summary>	Poisons an item, moving it from the pending state to the poisoned state. </summary>
+		/// <remarks>	Should throw an exception if no item exists in the pending state. </remarks>
+		/// <exception cref="ArgumentNullException">	Thrown when the item or poisonedItem are null. </exception>
+		/// <param name="item">		   	The original item. </param>
+		/// <param name="poisonedItem">	The original item, converted to its poisoned representation. </param>
+		/// <returns>	true if it succeeds, false if it fails. </returns>
+		public bool Poison(TQueue item, TQueuePoison poisonedItem)
+		{
+			if (null == item) { throw new ArgumentNullException("item"); }
+			if (null == poisonedItem) { throw new ArgumentNullException("poisonedItem"); }
+
+			lock (queue)
+			{
+				int index = pending.FindIndex(p => itemComparer.Equals(p, item));
+				if (-1 == index)
+				{
+					return false;
+				}
+				pending.RemoveAt(index);
+				poisoned.Add(poisonedItem);
+				return true;
+			}
+		}
+
+		/// <summary>	Deletes the given item from the poisoned state. </summary>
+		/// <remarks>	7/27/2011. </remarks>
+		/// <exception cref="ArgumentNullException">	Thrown when the poisonedItem is null. </exception>
+		/// <param name="poisonedItem">	The poisoned representation of an item. </param>
+		/// <returns>	true if it succeeds, false if it fails. </returns>
+		public bool Delete(TQueuePoison poisonedItem)
+		{
+			if (null == poisonedItem) { throw new ArgumentNullException("poisonedItem"); }
+
+			lock (queue)
+			{
+				int index = poisoned.FindIndex(poison => poisonComparer.Equals(poison, poisonedItem));
+				if (-1 == index)
+				{
+					return false;
+				}
+				poisoned.RemoveAt(index);
+				return true;
+			}
+		}
+
+		/// <summary>	Returns all poisoned items stored for this queue. </summary>
+		/// <remarks>	7/27/2011. </remarks>
+		/// <returns>	An enumerable collection of poisoned items (that may be empty). </returns>
+		public IEnumerable<TQueuePoison> GetPoisoned()
+		{
+			return poisoned.AsEnumerable();
+		}
+
+		/// <summary>	Returns all queue items stored for this queue. </summary>
+		/// <remarks>	7/27/2011. </remarks>
+		/// <returns>	An enumerable collection of queue items (that may be empty). </returns>
+		public IEnumerable<TQueue> GetQueued()
+		{
+			return queue.AsEnumerable();
+		}
+
+		/// <summary>	Returns all pending items stored for this queue. </summary>
+		/// <remarks>	7/27/2011. </remarks>
+		/// <returns>	An enumerable collection of pending items (that may be empty). </returns>
+		public IEnumerable<TQueue> GetPending()
+		{
+			return pending.AsEnumerable();
+		}
+	}
+}
